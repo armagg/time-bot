cb10861b21e8ecb0c898e8b99dd66dac0826611c
omid <omid.mesgarha@gmail.com>
2021-09-08 13:32:21 +0430

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/armagg/time_bot
+
+go 1.17
+
+require github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.1
---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/callbackquery"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/message"
+)
+
+func main(){
+
+}
+
+
+func main1() {
+	// Create bot from environment value.
+	b, err := gotgbot.NewBot(os.Getenv("API_TOKEN"), &gotgbot.BotOpts{
+		Client:      http.Client{},
+		GetTimeout:  gotgbot.DefaultGetTimeout,
+		PostTimeout: gotgbot.DefaultPostTimeout,
+	})
+	if err != nil {
+		panic("failed to create new bot: " + err.Error())
+	}
+
+	// Create updater and dispatcher.
+	updater := ext.NewUpdater(&ext.UpdaterOpts{
+		ErrorLog: nil,
+		DispatcherOpts: ext.DispatcherOpts{
+			Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+				fmt.Println("an error occurred while handling update:", err.Error())
+				return ext.DispatcherActionNoop
+			},
+			Panic:       nil,
+			ErrorLog:    nil,
+			MaxRoutines: 0,
+		},
+	})
+	dispatcher := updater.Dispatcher
+
+	// Add echo handler to reply to all messages.
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
+	dispatcher.AddHandler(handlers.NewCommand("source", source))
+	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Equal("start_callback"), startCB))
+	dispatcher.AddHandler(handlers.NewMessage(message.All, echo))
+
+	// Start receiving updates.
+	err = updater.StartPolling(b, &ext.PollingOpts{DropPendingUpdates: true})
+	if err != nil {
+		panic("failed to start polling: " + err.Error())
+	}
+	fmt.Printf("%s has been started...\n", b.User.Username)
+
+	// Idle, to keep updates coming in, and avoid bot stopping.
+	updater.Idle()
+}
+
+func source(b *gotgbot.Bot, ctx *ext.Context) error {
+	f, err := os.Open("samples/echoBot/main.go")
+	if err != nil {
+		return fmt.Errorf("failed to open source: %w", err)
+	}
+
+	_, err = b.SendDocument(ctx.EffectiveChat.Id, f, &gotgbot.SendDocumentOpts{
+		Caption:          "Here is my source code.",
+		ReplyToMessageId: ctx.EffectiveMessage.MessageId,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to send source: %w", err)
+	}
+
+	// Alternative file sending solutions:
+
+	// --- By file_id:
+	// _, err = ctx.Bot.SendDocument(ctx.EffectiveChat.Id, "file_id", &gotgbot.SendDocumentOpts{
+	//	Caption:          "Here is my source code.",
+	//	ReplyToMessageId: ctx.EffectiveMessage.MessageId,
+	// })
+	// if err != nil {
+	//	return fmt.Errorf("failed to send source: %w", err)
+	// }
+
+	// --- By []byte:
+	// bs, err := ioutil.ReadFile("samples/echoBot/main.go")
+	// if err != nil {
+	//	return fmt.Errorf("failed to open source: %w", err)
+	// }
+	//
+	// _, err = ctx.Bot.SendDocument(ctx.EffectiveChat.Id, bs, &gotgbot.SendDocumentOpts{
+	//	Caption:          "Here is my source code.",
+	//	ReplyToMessageId: ctx.EffectiveMessage.MessageId,
+	// })
+	// if err != nil {
+	//	return fmt.Errorf("failed to send source: %w", err)
+	// }
+
+	// --- By custom name:
+	// f2, err := os.Open("samples/echoBot/main.go")
+	// if err != nil {
+	//	return fmt.Errorf("failed to open source: %w", err)
+	//	return err
+	// }
+	//
+	// _, err = ctx.Bot.SendDocument(ctx.EffectiveChat.Id, gotgbot.NamedFile{
+	//	File:     f2,
+	//	FileName: "NewFileName",
+	// }, &gotgbot.SendDocumentOpts{
+	//	Caption:          "Here is my source code.",
+	//	ReplyToMessageId: ctx.EffectiveMessage.MessageId,
+	// })
+	// if err != nil {
+	//	return fmt.Errorf("failed to send source: %w", err)
+	//	return err
+	// }
+
+	return nil
+}
+
+// start introduces the bot.
+func start(b *gotgbot.Bot, ctx *ext.Context) error {
+	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, I'm @%s. I <b>repeat</b> all your messages.", b.User.Username), &gotgbot.SendMessageOpts{
+		ParseMode: "html",
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+				{Text: "Press me", CallbackData: "start_callback"},
+			}},
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to send start message: %w", err)
+	}
+	return nil
+}
+
+// startCB edits the start message.
+func startCB(b *gotgbot.Bot, ctx *ext.Context) error {
+	cb := ctx.Update.CallbackQuery
+
+	_, err := cb.Answer(b, nil)
+	if err != nil {
+		return fmt.Errorf("failed to answer start callback query: %w", err)
+	}
+
+	_, err = cb.Message.EditText(b, "You edited the start message.", nil)
+	if err != nil {
+		return fmt.Errorf("failed to edit start message text: %w", err)
+	}
+	return nil
+}
+
+// echo replies to a messages with its own contents.
+func echo(b *gotgbot.Bot, ctx *ext.Context) error {
+	_, err := ctx.EffectiveMessage.Reply(b, ctx.EffectiveMessage.Text, nil)
+	if err != nil {
+		return fmt.Errorf("failed to echo message: %w", err)
+	}
+	return nil
+}
